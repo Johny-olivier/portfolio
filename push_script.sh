6e4abf02a4df32817ec2f792bfde7feff4b86709
Johny Olivier <johny24.poseidon@gmail.com>
2026-06-30 19:29:42 +0300

    Delete du fichier push_script

--- push_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-TOKEN=$(cat /home/johny-olivier/token_github.txt)
-git remote set-url origin "https://Johny-olivier:${TOKEN}@github.com/Johny-olivier/portfolio.git"
-git push -u origin main
